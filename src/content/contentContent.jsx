fc82c027d5ee1a11f42ee38fc1fd5ff88324c8ea
AndrewMedvedev42 <sanjino56@gmail.com>
2021-03-31 17:45:19 +0300

    finished the Contact section

--- src/content/contentContent.jsx
+++ src/content/contentContent.jsx
@@ -2,6 +2,14 @@ function ContactContent(){
     return(
         <section>
                 <h1 className="titleText">Contacts</h1>
+                <div className="contactContainer">
+                    <div className="container">
+                        <h3>example@dummyEmail.com</h3>
+                    </div>
+                    <div className="container">
+                        <h3>+fakePhoneNumber</h3>
+                    </div>
+                </div>
         </section>
         
     )
